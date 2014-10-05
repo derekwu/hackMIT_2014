cd863e7fea00e9c6fb7346dd71e1d2ff5c711c51
Derek Wu <derekwu@Dereks-MacBook-Pro.local>
2014-10-04 21:21:52 -0400

    made board

--- musicboard.js
+++ musicboard.js
@@ -1,15 +1,16 @@
-function music_board(size) {
-	var board_state = new Array(size);
-	for (i = 0; i < size; i++) {
-		board_state[i] = new Array(size);
+function music_board(size_int) {
+	this.size = size_int
+	var board_state = new Array(size_int);
+	for (i = 0; i < size_int; i++) {
+		board_state[i] = new Array(size_int);
 	}
 
 	// Populate board with random values
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
+    for (i = 0; i < size_int; i++) {
+        for (j = 0; j < size_int; j++) {
             board_state[i][j] = Math.floor((Math.random() * 2));
         }
     }
 
-    this.board = board_state
+    this.board = board_state;
 }
