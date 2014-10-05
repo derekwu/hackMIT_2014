dcfb57db80c076ac79679393dd38bc95496a8cfd
Derek Wu <derekwu@Dereks-MacBook-Pro.local>
2014-10-04 20:29:14 -0400

    made backend board

--- musicboard.js
+++ musicboard.js
@@ -1,3 +1,15 @@
 function music_board(size) {
-	
+	var board = new Array(size);
+	for (i = 0; i < size; i++) {
+		board[i] = new Array(size);
+	}
+
+	// Populate board with random values
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            board[i][j] = Math.floor((Math.random() * 2));
+        }
+    }
+
+    this.board_state = board
 }
